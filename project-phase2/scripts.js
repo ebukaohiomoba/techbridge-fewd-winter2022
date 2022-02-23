7d90fba810467c230ccd9ac6e239901c5e620935
Ebuka Ohiomoba <chukwuebukaohiomoba@gmail.com>
2022-02-23 13:19:44 -0800

    Fixed grid function

--- project-phase2/scripts.js
+++ project-phase2/scripts.js
@@ -1,4 +1,6 @@
 //scripts for problems
+
+
 function resetFindInGrid() {
     //set tbNumberToFind to blank
     let tbNumberToFind = document.getElementById("tbNumberToFind");
@@ -34,13 +36,68 @@ function reloadPage() {
 
 
 function findInGrid() {
- //find in grid
+
+    //Grabs rows in the grid
+    let tbNumberToFind = document.getElementById("tbNumberToFind").value;
+    console.log(tbNumberToFind);
+    let gridRows = document.getElementsByClassName("row");
+    for(let row = 0; row < gridRows.length; row++){
+        //Collects the spans from a row 
+        let spanofgridRow = gridRows[row].getElementsByClassName("spanName");
+        console.log(spanofgridRow);
+        console.log(spanofgridRow.length);
+
+        //loop through each span in the collection
+        for(let i = 0; i < spanofgridRow.length; i++) {
+            //log the row
+            console.log("Span collection: ", spanofgridRow[i]);
+            let spanText = spanofgridRow[i].innerText;
+            console.log("spanText: ", spanText);
+            if(spanText == tbNumberToFind) {
+                spanofgridRow[i].classList.add('spanHighlighted');
+            }  else {
+                spanofgridRow[i].classList.remove('spanHighlighted');
+
+            }
+        }
+
+    }
+
+
+
 }
 
+
 function findInList() {
-   //find In List
-}
+//Grabs the number that user enters in input box
+let tbTextToFind = document.getElementById("tbTextToFind").value;
+console.log(tbNumberToFind);
+let gridRows = document.getElementsByClassName("row");
+for(let row = 0; row < gridRows.length; row++){
+    //Collects the spans from a row 
+    let gridRow = gridRows[row].getElementsByClassName("spanName");
+    console.log(gridRow);
+    console.log(gridRow.length);
+
+    //loop through each span in the collection
+    for(let i = 0; i < gridRow.length; i++) {
+        //log the row
+        console.log("Span collection: ", gridRow[i]);
+        //get the id of the span
+        //let spanID = gridRow[i].id;
+        //console.log("spanID: ", spanID);
+
+        let spanText = gridRow[i].innerText;
+        console.log("spanText: ", spanText);
+        if(spanText == tbNumberToFind) {
+            //document.getElementById("span3-1").className = 'spanHighlighted'
+            //document.getElementsByClassName("spanName").className = 'spanHighlighted';
+           
+        }
+
+    }}
 
 function sortIt() {
     //
+}
 }
